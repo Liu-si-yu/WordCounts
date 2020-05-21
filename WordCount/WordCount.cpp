e3650428c86b195e67430054eff58734b7467375
刘思宇 <3014739687@qq.com>
2020-05-21 22:06:14 +0800

    提交

---
+++ WordCount/WordCount.cpp
@@ -0,0 +1,18 @@
+// WordCount.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "stdio.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	int sum = 0;
+	for (int i = 0; i <= 8; i++)
+	{
+		sum += i;
+	}
+	printf("%d\n", sum);
+	return 0;
+}
+
